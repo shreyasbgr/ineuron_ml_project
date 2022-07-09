c0cecd5c6b8a34a61b468565e14f451035217882
Shreyas Banagar <shreyas.banagar@zycus.com>
2022-07-09 21:00:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def index():
-    return "Starting ML Project."
+    return "Starting ML Project. CI/CD pipeline established!"
 
 if __name__ == '__main__':
     app.run(debug=True)
