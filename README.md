f17f05ebc6789288ef4a74d12c6e01d954ad715d
Shreyas Banagar <shreyas.banagar@zycus.com>
2022-08-02 18:56:02 +0530

    Add setup.py

--- README.md
+++ README.md
@@ -17,3 +17,12 @@ Stop Docker container
 docker ps
 docker stop <container-id>
 ```
+
+VSCode Workspace settings.json file:
+```
+{
+    "terminal.integrated.defaultProfile.windows": "Command Prompt",
+    "python.PYTHONPATH": "C:\\Users\\shreyas.banagar\\OneDrive - Zycus\\iNeuron\\Repos\\ineuron_ml_project\\venv\\python.exe",
+    "python.terminal.activateEnvironment": true,
+}
+```
---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+from typing import List
+
+#Declaring variables for setup function
+PROJECT_NAME="housing-predictor"
+VERSION="0.0.1"
+AUTHOR="Shreyas Banagar"
+DESCRIPTION="This is the first ML end-to-end ML project of iNeuron"
+PACKAGES=["housing"]
+REQUIREMENTS_FILE_NAME="requirements.txt"
+
+def get_requirements_list() -> List[str]:
+    """
+    Description: This function reads the requirements.txt file and returns a list of strings having all the package requirements for the project.
+    return type: List[str]
+    """
+    with open(REQUIREMENTS_FILE_NAME) as requirement_file:
+        return requirement_file.readlines()
+
+
+
+
+setup(
+    name=PROJECT_NAME,
+    version=VERSION,
+    author=AUTHOR,
+    description=DESCRIPTION,
+    packages=PACKAGES,
+    install_requires=get_requirements_list()
+)
+
+if __name__ == "__main__":
+    print(get_requirements_list())
